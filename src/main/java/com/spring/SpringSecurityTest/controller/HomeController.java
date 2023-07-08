f6c0702edd0b0c40d8976e4166a407e951339e75
bishwamohan14 <bishwamohan14@gmail.com>
2023-07-08 21:04:46 +0530

    Spring security old practice set

---
+++ src/main/java/com/spring/SpringSecurityTest/controller/HomeController.java
@@ -0,0 +1,32 @@
+package com.spring.SpringSecurityTest.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+
+
+    @PreAuthorize("hasRole('NORMAL')")
+    @GetMapping("/normal")
+    public ResponseEntity<String> normalUser(){
+        return ResponseEntity.ok("Yes,I am normal User");
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public ResponseEntity<String> adminUser(){
+        return ResponseEntity.ok("Yes,I am admin User");
+    }
+
+    @GetMapping("/public")
+    public ResponseEntity<String> publicUser(){
+        return ResponseEntity.ok("Yes,I am public  User");
+    }
+
+}
